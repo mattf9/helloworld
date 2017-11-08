483d6721583cb1452c2eb1e98e409752a35ec45f
nex9 <nex9@core.pri>
2017-11-07 22:40:29 -0500

    jenkins github test

--- docker/app/main.py
+++ docker/app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask"
+    return "Hello Jenkins from Flask"
 
 if __name__ == "__main__":
     # Only for debugging while developing
