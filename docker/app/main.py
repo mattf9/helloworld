771e51e5970b62f86c01c572dce4e35ad2402b5f
nex9 <nex9@core.pri>
2017-11-08 20:31:00 -0500

    first pass at docker image build pipeline step

--- docker/app/main.py
+++ docker/app/main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Jenkins from Flask"
+    return "Docker build v2.0"
 
 if __name__ == "__main__":
     # Only for debugging while developing
